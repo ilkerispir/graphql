40728d7d0ef1c9f6a149de0ddc56452447d8cd1b
Ilker <ikerispir@outlook.com>
2020-10-04 01:19:04 +0300

    feat: added express.js in graphql

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var { graphql, buildSchema } = require('graphql');
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
   type Query {
@@ -8,6 +10,10 @@ var schema = buildSchema(`
 
 var root = { hello: () => 'Hello world!' };
 
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
-});
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
